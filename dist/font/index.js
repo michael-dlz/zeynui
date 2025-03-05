50cf67820d5d760b0256950103aace1bf1de9def
michael-dlz <105244377+michael-dlz@users.noreply.github.com>
2025-03-05 14:33:59 -0500

    update

--- dist/font/index.js
+++
@@ -1,5 +0,0 @@
-import { Plus_Jakarta_Sans } from "next/font/google";
-export const defaultFont = Plus_Jakarta_Sans({
-    subsets: ["latin"],
-    weight: ["200", "300", "400", "500", "600", "700", "800"],
-});
